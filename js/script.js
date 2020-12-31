b596381f6b76178cf6570b270a06254e9f1b17d5
Gabriel Souza <azevedoedit@gmail.com>
2020-12-31 19:00:30 -0300

    initiated js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelectorAll("#message");
+let messageText = document.querySelector("#message p");
+let secondPlayer;
